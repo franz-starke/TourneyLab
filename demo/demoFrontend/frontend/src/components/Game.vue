7f0cbfdfe20428b4211f2d4c2d6cb4de84989660
franz-starke <franzstarke@yahoo.com>
2025-03-16 15:06:39 +0100

    Moved and removed demos to demo folder

---
+++ demo/demoFrontend/frontend/src/components/Game.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    game
+</template>
+
+<style scoped>
+
+</style>
---
+++ demo/demoFrontend/frontend/src/components/GameBlock.vue
@@ -0,0 +1,23 @@
+<script setup>
+    import Game from './Game.vue';
+
+    const props = defineProps({
+    time: {
+        type: String,
+        required: true
+    },
+    games: {
+        type: Array,
+        required: true
+    }
+    });
+</script>
+
+<template>
+    {{ time }}
+    <Game v-for="game in games"/>
+</template>
+
+<style scoped>
+
+</style>
