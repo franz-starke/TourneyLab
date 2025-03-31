8652d9c0c67db639f8f904367845645c6fe659ae
franz-starke <franzstarke@yahoo.com>
2025-03-31 23:40:44 +0200

    Deleted unused api objects

--- src/backend/data/apiobjects/apiobjects.py
+++ src/backend/data/apiobjects/apiobjects.py
@@ -1,45 +1,12 @@
 from pydantic import BaseModel
 
-class TeamInfo(BaseModel):
-    TeamID: str
-    Team_Name: str
+class CreateTournament(BaseModel):
+    name: str
+    fields: int
+    teams: int
+    groups: int
+    returngame: bool
 
-class PunkteInfo(BaseModel):
-    Punkte_Team1: int
-    Punkte_Team2: int
-
-class GameInfo(BaseModel):
-    SpielID: str
-    Anfang: int
-    Ende: int
-    Team1: TeamInfo
-    Team2: TeamInfo
-    Schiri: str
-    Punkte: PunkteInfo
-
-class CreateTurnier(BaseModel):
-    Turniername: str
-    Anzahl_der_Felder: int
-    Anzahl_der_Teams: int
-    Anzahl_der_Leistungsgruppen: int
-    Hin_Rueckspiel: bool
-
-class PunkteUpdate(BaseModel):
-    Punkte_Team1: int
-    Punkte_Team2: int
-
-class GroupInfo(BaseModel):
-    Name: str
-    GruppenID: str
-
-class TeamGroupEntry(BaseModel):
-    Name: str
-    Spielpunkten: int
-
-class TeamGameInfo(BaseModel):
-    Team1: str
-    Team2: str
-    Anfang: int
-    Ende: int
-    Feld: int
-    Punkte: PunkteInfo
+class PointUpdate(BaseModel):
+    team1: int
+    team2: int
