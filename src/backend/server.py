df983e2d14fd1e1a71c7766d9d45204d8d6f85ee
franz-starke <franzstarke@yahoo.com>
2025-03-31 23:40:59 +0200

    Added function for api server interaction

---
+++ src/backend/server.py
@@ -0,0 +1,41 @@
+import os
+import sqlite3
+
+class Server:
+    def __init__(self):
+        pass
+
+    def query(self,tournament_id="",query="",attributes=[]):
+        try:
+            with sqlite3.connect(os.path.join("data",f"{tournament_id}.db")) as connection:
+                cursor = connection.cursor()
+                cursor.execute(query,attributes)
+                data = cursor.fetchall()
+                return data
+
+        except Exception as e:
+            print(f"Error executing query: {e}")
+
+    def create_tournaments(self):
+        pass
+
+    def get_tournaments(self):
+        pass
+
+    def get_field_games(self):
+        pass
+
+    def get_game_points(self):
+        pass
+
+    def update_game_points(self):
+        pass
+
+    def get_groups(self):
+        pass
+
+    def get_group_teams(self):
+        pass
+
+    def get_team_info(self):
+        pass
