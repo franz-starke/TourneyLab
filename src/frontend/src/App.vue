3c516ad9e78056f692037e58ae590de8508499aa
joshua-cloud-dot <joshua.heninger@posteo.de>
2025-03-31 13:35:15 +0200

    deleted styles from vue init starting to add own styles

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -6,22 +6,6 @@ import { RouterLink, RouterView } from 'vue-router'
 <RouterView />
 </template>
 
-<style >
+<style scoped>
 
-* {
-  text-align: center;
-}
-a {
-    display: block;
-    background-color: black;
-    border: solid 1px black;
-    color: white;
-    padding: 50px;
-    margin: 10px;
-    font-size: xx-large;
-  }
-  a:hover,
-  a.router-link-active {
-    background-color: rgb(110, 79, 13);
-  }
 </style>
--- src/frontend/src/components/TournamentHome.vue
+++ src/frontend/src/components/TournamentHome.vue
@@ -17,25 +17,7 @@
 
 
 <style scoped>
-
-    #header, #navbar {
-        background-color: gray;
-        padding: 5%;
-    }
-
-    a {
-        display: inline-block;
-        background-color: black;
-        border: solid 1px black;
-        color: white;
-        padding: 50px;
-        margin: 10px;
-        font-size: xx-large;
-    }
-    a:hover,
-    a.router-link-active {
-        background-color: rgb(110, 79, 13);
-    }
+   
 </style>
 
 
