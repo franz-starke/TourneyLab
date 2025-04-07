8b8d8812f49a698a8806e4fc8efcaa5cafa185e1
joshua-cloud-dot <joshua.heninger@posteo.de>
2025-04-07 18:19:39 +0200

    Component: CreateTournament.vue got elements to input and a button with a method to send new tournament parameters. Method not implemented yet

--- src/frontend/src/components/CreateTournament.vue
+++ src/frontend/src/components/CreateTournament.vue
@@ -1,8 +1,42 @@
-<script setup> </script>
+<script>
+  export default {
+    data() {
+      return {
+        tournamentName: "",
+        anzahlFelder: 1,
+        anzahlTeams: 3,
+        anzahlGruppen: 1,
+        mitHinUndRueckSpiel: true,
+      }
+    },
+    methods: {
+      createTournament() {
+        console.log(this.tournamentName);
+
+        console.log(this.anzahlFelder);
+        console.log(this.anzahlTeams);
+        console.log(this.anzahlGruppen);
+        console.log(this.mitHinUndRueckSpiel);
+        console.log("createTournament not implementet yet");
+      },
+
+    }
+  };
+</script>
 
 <template>
-
-  <h1>Create Tournament Not Implemented</h1>
-
+  <form>
+    <input v-model="tournamentName" type="text" placeholder="Enter tournament name" required><br>
+    <p>Item name: <input v-model="anzahlFelder" type="number" min="1" max="4" required></p>
+    <p>How many: <input v-model="anzahlTeams" type="number" min="3" max="12" required></p>
+    <p>How many: <input v-model="anzahlGruppen" type="number" min="1" max="2" required></p>
+    <p>Hin- und Rückspiel <input v-model="mitHinUndRueckSpiel" type="checkbox" required></p>
+    <div class="highlight-button" @click="createTournament" type="submit">Create</div>
+  </form>
 </template>
+
+
+<style scoped>
+
+</style>
 
