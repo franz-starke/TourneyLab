432446abbd68b37d7cfc198e33be38897bfa8e50
joshua-cloud-dot <joshua.heninger@posteo.de>
2025-04-14 17:48:00 +0200

    added all api endpoints as specified in the backend docs
    requires testing as soon as backend runs

--- src/frontend/src/api/api.js
+++ src/frontend/src/api/api.js
@@ -1,4 +1,6 @@
 // module for calling the api endpoints 
+// see the backend documentation for more detail on the available endpoints
+// these methods will return the raw response
 import axios from "axios";
 
 
@@ -18,35 +20,46 @@ class api {
         console.log("Api requests go to: " + this.serverAddr);
     }
  
-    async createTournament(tournamentData) {
-        console.log("I GOT CALLED");
-        // try {
-        //     const response = await axios.post(`https://${serverAddr}/api/create"`, );
-        //   } catch (error) {
-        //     console.error('There was an error!', error);
-        //   } finally {
-        //     this.loading = false;
-        // }
+    async createTournament(tournamentData)  {
+        try {
+            return await axios.post(`https://${serverAddr}/api/create`, tournamentData);
+        } catch (error) {
+          console.error('API: ', error);
+        }
     }
- 
+
+    async getOldTournaments() {
+        try {
+            return await axios.get(`https://${serverAddr}/api/tournaments`);
+        } catch (error) {
+            console.error('There was an error!', error);
+        }
+    }
+
+    async getGamesWithScoresFromField(tournamentID, fieldID) {
+        try {
+            return await axios.get(`https://${serverAddr}/api/${tournamentID}/fields/${fieldID}`);
+        } catch (error) {
+            console.error('There was an error!', error);
+        }
+    }
 
 
+    async getGameScore(tournamentID, gameID) {
+        try {
+            return await axios.get(`https://${serverAddr}/api/${tournamentID}/game/${gameID}`);
+        } catch (error) {
+            console.error('There was an error!', error);
+        }
+    }
 
-
-
-// Get old Tournaments
-
-
-
-
-// Get game scores for field
-
-
-// Get specific game  score
-
-
-// POST game score
-
+    async editGameScore(tournamentID, gameID, score) {
+        try {
+            return await axios.post(`https://${serverAddr}/api/${tournamentID}/game/${gameID}`);
+        } catch (error) {
+            console.error('There was an error!', error);
+        }
+    }
 }
 
 export default new api(localhost);
