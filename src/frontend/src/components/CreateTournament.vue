dc81034385f5ad7fd2ecd358825867daa88289af
wittc <wittchenjannis2011@gmail.com>
2025-04-14 16:15:13 +0200

    implemented algo

--- src/frontend/src/components/CreateTournament.vue
+++ src/frontend/src/components/CreateTournament.vue
@@ -1,5 +1,6 @@
 <script>
   import axios from 'axios'
+  import { createTournamentAlgo } from '../tournamentalgo/tournamentalgo.js';
 
     
   
@@ -14,15 +15,12 @@
         "groups": this.amountGroups,
         "return": this.withReturnGame,
         "refs": {
-
         },
         "games": {
-
         }
       }
 
       try {
-
         const response = await axios.post("https://htw-turnier.de/api/create", );
       } catch (error) {
         console.error('There was an error!', error);
@@ -46,33 +44,11 @@
       return {
         tournamentName: "",
         amountFields: 1,
-        amountTeams: 3,
+        amountTeams1: 4,
+        amountTeams2: 4,
         amountGroups: 1,
-        withReturnGame: true,
+        withReturnGame: false,
         tournamentData: {},
-
-
-        // FIXME: TESTING
-        //////////////////
-        gamesTest: {
-          1: [
-            // game// possible refs
-            [1,2,3],
-            [2,3,null],
-            [3,2,null],
-          ],
-          2: [
-            [4,5,9],
-            [8,6,4],
-            [5,7,null],
-          ],
-        },
-        freeRefsPerRoundTest: {
-          2: [1,5,7],
-          3: [1,4,6],
-        },
-        //////////////////
-        
 
         games: {},
         showRefModal: false,
@@ -82,34 +58,19 @@
       async generateTournament() {
         // TODO:
         //  let games = {}
-        //  games = await derNervigsteSagenumwobeneKannMichMalFettAmA****LeckenAlgorithmus(this.amountFields, this.amountTeams, this.amountGroups);
-
+        this.games = await createTournamentAlgo(this.amountTeams1, this.amountTeams2 , this.amountGroups, this.amountFields, this.withReturnGame);
         // TODO: 
         // based on output from the Tournament Algorith, we need to decide if refs have to be added manually
         // if (games has games with refs == null) then:
         this.games = await this.openRefModal();
         // else 
-
-
         console.log(this.games);
-
-
 
       },      
       openRefModal() {
         return new Promise((resolve) => {
           this.showRefModal = true; 
-
-          // do something with this.games
-          
-
-
-          // Function to handle form submission
           this.submitForm = () => {
-            // FIXME: TESTING
-            /////////
-            this.games = this.gamesTest
-            //////////
             this.showRefModal = false;
             resolve(this.games); // Resolve the promise with the user input
           };
@@ -128,8 +89,6 @@
       updateFreeRefs($event, round) {
           // TODO: Implement that if choosing refs from one Round, to add back to Round who was selected before (if so), and remove next selected
       }
-
-
     }
   };
 </script>
@@ -138,7 +97,8 @@
   <form v-if="!showRefModal">
     <input v-model="tournamentName" type="text" placeholder="Enter tournament name" required><br>
     <p>Amount Fields: <input v-model="amountFields" type="number" min="1" max="4" required></p>
-    <p>Amount Teams: <input v-model="amountTeams" type="number" min="3" max="12" required></p>
+    <p>Amount Teams1: <input v-model="amountTeams1" type="number" min="3" max="12" required></p>
+    <p>Amount Teams2: <input v-model="amountTeams2" type="number" min="3" max="12" required></p>
     <p>Amount Groups: <input v-model="amountGroups" type="number" min="1" max="2" required></p>
     <p>Return Game <input v-model="withReturnGame" type="checkbox" required></p>
     <div class="highlight-button" @click="generateTournament" type="submit">Create</div>
@@ -150,33 +110,26 @@
      then assign them  -->
 
 
-    <div v-for="(field, fieldNum) in gamesTest "> 
+    <div v-for="(field, fieldNum) in games "> 
       Field {{ fieldNum }}
       <div v-for="(game, round) in field">
-        <div v-if="game[2] == null">
-          Round {{ round }}: Team {{ game[0] }} vs. Team {{ game[1] }} Ref =
-          <select @change="updateFreeRefs($event, round)" name="possible-refs">
-            <option  v-for="ref in freeRefsPerRoundTest[round + 1]" :value="ref" >{{ ref }}</option>
+        <div v-if="!game[2] || game[2] === 'leer'">
+          Round {{ round }}: Team {{ game[0] }} vs. Team {{ game[1] }}  
+          <br>
+          Referee:
+          <select @change="updateFreeRefs($event, round, fieldNum)">
+            <option v-for="ref in [1, 2, 3, 4, 5]" :key="ref" :value="ref">
+              {{ ref }}
+            </option>
           </select>
-
         </div>
       </div>
     </div>
-
-
     <button @click="submitForm">save refs</button>
     <button @click="closeModal">cancel</button>
-
-
   </div>
 
-
 </template>
-
-
 <style scoped>
-
-
-
 </style>
 
