c046caf2c50c841a6dc33ee1bc741c3847fc3138
franz-starke <franzstarke@yahoo.com>
2025-04-30 12:04:09 +0200

    Added function to database class to handle queries in separate tournament files

--- src/backend/database.py
+++ src/backend/database.py
@@ -6,22 +6,13 @@ class Database:
     def __init__(self):
         pass
 
-def create_tournament_db(tournament_id: str):
-    # Stelle sicher, dass der Ordner für Datenbanken existiert
-    db_folder = "databases.py"
+    def query(self,tournament_id="",query="",attributes=[]):
+        try:
+            with sqlite3.connect(os.path.join(DATABASE_PATH,f"{tournament_id}.db")) as connection:
+                cursor = connection.cursor()
+                cursor.execute(query,attributes)
+                data = cursor.fetchall()
+                return data
+        except Exception as e:
+            print(f"Error executing query: {e}")
 
-    # Erstelle Pfad zur neuen DB
-    db_path = os.path.join(db_folder, f"{tournament_id}.db")
-
-
-    conn = sqlite3.connect(db_path)
-    cursor = conn.cursor()
-
-    # Tabellenstruktur
-    cursor.execute('''
-
-    ''')
-
-
-    conn.commit()
-    conn.close()
