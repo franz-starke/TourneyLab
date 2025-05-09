a6ee79d25c9ff91bd24802f30f884f17f5b283d4
franz-starke <franzstarke@yahoo.com>
2025-05-09 12:47:26 +0200

    Added types to apiobjects

--- src/backend/data/apiobjects/apiobjects.py
+++ src/backend/data/apiobjects/apiobjects.py
@@ -2,9 +2,9 @@ from pydantic import BaseModel
 
 class CreateTournament(BaseModel):
     name: str
-    teams: dict
-    games: dict
+    teams: dict[str,int]
+    games: dict[str,dict[str,list[str]]]
     date: int
 
 class ScoreUpdate(BaseModel):
-    score: list
+    score: list[int]
