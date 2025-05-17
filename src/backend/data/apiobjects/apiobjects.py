e107a3c67dbf522af7596f568f3b2c552bee996c
IbrahimBraikehD <147654018+IbrahimBraikehD@users.noreply.github.com>
2025-05-17 16:14:44 +0200

    addad validation for score

--- src/backend/data/apiobjects/apiobjects.py
+++ src/backend/data/apiobjects/apiobjects.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
+from typing import List
 
 class CreateTournament(BaseModel):
     name: str
@@ -8,3 +9,13 @@ class CreateTournament(BaseModel):
 
 class ScoreUpdate(BaseModel):
     score: list[int]
+
+    @validator("score")
+    def validate_score(cls, v):
+        if not isinstance(v, list) or len(v) != 2:
+            raise ValueError("score must be a list of exactly two integers.")
+        if not all(isinstance(x, int) for x in v):
+            raise ValueError("both values in score must be integers.")
+        if any(x < 0 for x in v):
+            raise ValueError("score values must be non-negative.")
+        return v
