85a588d62168a3d7b634a7f4cc34eb3f87736ebe
franz-starke <franzstarke@yahoo.com>
2025-05-17 14:07:22 +0200

    Quickfix for 422 error code

--- src/backend/data/apiobjects/apiobjects.py
+++ src/backend/data/apiobjects/apiobjects.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 class CreateTournament(BaseModel):
     name: str
     teams: dict[str,int]
-    games: dict[str,dict[str,list[str]]]
+    games: dict
     date: str
 
 class ScoreUpdate(BaseModel):
