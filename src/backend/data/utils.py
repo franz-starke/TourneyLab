5e2869a88af846f18383fd756c5aa7fd75b1391e
franz-starke <franzstarke@yahoo.com>
2025-05-19 20:55:31 +0200

    Added colors for terminal prints

--- src/backend/data/utils.py
+++ src/backend/data/utils.py
@@ -7,3 +7,17 @@ class Error:
     def __init__(self,code:int,message:str) -> None:
         self.code = code
         self.message = message
+
+class Color:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def log(message:str|Exception):
+    print(str(message))
