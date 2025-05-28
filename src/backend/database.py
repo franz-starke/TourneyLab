f0ec8b6b96ecbd98ff8be2ee22442ac095679eed
Ruben-David Kraus <ruben-david.kraus@ottogroup.com>
2025-05-28 12:15:48 +0200

    fix: refactored return types

--- src/backend/database.py
+++ src/backend/database.py
@@ -1,9 +1,11 @@
 import os
 import sqlite3
 import data.utils as utils
+from typing import Union, List, Dict
+
 
 class Database:
-    def query(self, tournament_id="", query="", attributes=[]) -> list|bool|None:
+    def query(self, tournament_id="", query="", attributes=[]) -> Union[List, bool, None]:
         """
         Executes a SQL query on the tournaments database.
 
@@ -21,7 +23,7 @@ class Database:
         try:
             if not os.path.exists(os.path.join(utils.DATABASE_PATH, f"{tournament_id}.db")):
                 return None
-            
+
             with sqlite3.connect(os.path.join(utils.DATABASE_PATH, f"{tournament_id}.db")) as connection:
                 cursor = connection.cursor()
                 cursor.execute(query, attributes)
@@ -41,23 +43,22 @@ class Database:
                 return None
 
         except Exception:
-            utils.LOGGER.error(f"Cannot process query: {tournament_id}, {query}, {attributes}.")
+            utils.LOGGER.error(
+                f"Cannot process query: {tournament_id}, {query}, {attributes}.")
             return None
 
-
     def create_tournament(
-        self,
-        tournament_id: str,
-        name: str,
-        date: str,
-        field_count: int,
-        team_count: int,
-        group_count: int,
-        fields: dict[str, str],
-        teams: dict[str, list[str|int]],
-        groups: dict[str, str],
-        games: dict) -> bool|None:
-
+            self,
+            tournament_id: str,
+            name: str,
+            date: str,
+            field_count: int,
+            team_count: int,
+            group_count: int,
+            fields: dict[str, str],
+            teams: dict[str, list[Union[str, int]]],
+            groups: dict[str, str],
+            games: dict) -> Union[bool, None]:
         """
         Initializes the database for a new tournament and inserts the provided data.
 
@@ -91,8 +92,8 @@ class Database:
         result.append(self.query(tournament_id, """
             INSERT INTO config 
             (id, name, date, field_count, team_count, group_count)
-            VALUES (?, ?, ?, ?, ?, ?)""", 
-            [tournament_id, name, date, field_count, team_count, group_count]))
+            VALUES (?, ?, ?, ?, ?, ?)""",
+                                 [tournament_id, name, date, field_count, team_count, group_count]))
 
         result.append(self.query(tournament_id, """
             CREATE TABLE IF NOT EXISTS groups (
@@ -100,8 +101,8 @@ class Database:
             name TEXT)"""))
 
         for group_id, group_name in groups.items():
-            result.append(self.query(tournament_id, """INSERT INTO groups (id, name) VALUES (?, ?)""", 
-                [group_id, group_name]))
+            result.append(self.query(tournament_id, """INSERT INTO groups (id, name) VALUES (?, ?)""",
+                                     [group_id, group_name]))
 
         result.append(self.query(tournament_id, """
             CREATE TABLE IF NOT EXISTS teams (
@@ -111,7 +112,7 @@ class Database:
 
         for team_id, (team_name, group_id) in teams.items():
             result.append(self.query(tournament_id, """INSERT INTO teams (id, name, group_id) VALUES (?, ?, ?)""",
-                [team_id, team_name, group_id]))
+                                     [team_id, team_name, group_id]))
 
         result.append(self.query(tournament_id, """
             CREATE TABLE IF NOT EXISTS fields (
@@ -120,7 +121,7 @@ class Database:
 
         for field_id, field_name in fields.items():
             result.append(self.query(tournament_id, """INSERT INTO fields (id, name) VALUES (?, ?)""",
-                [field_id, field_name]))
+                                     [field_id, field_name]))
 
         result.append(self.query(tournament_id, """CREATE TABLE IF NOT EXISTS games (
             id       TEXT PRIMARY KEY,
@@ -136,47 +137,41 @@ class Database:
             result.append(self.query(tournament_id, """
                 INSERT INTO games 
                 (id, field_id, team1, team2, ref, time, score1, score2)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?)""", 
-                [game_id, *game_data]))
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?)""",
+                                     [game_id, *game_data]))
 
         for variable in result:
             if variable == None:
                 return None
-            
+
         return True
 
-
-    def get_config(self, tournament_id: str) -> list|bool|None:
+    def get_config(self, tournament_id: str) -> Union[List, bool, None]:
         """Returns the config of a tournament."""
 
         return self.query(tournament_id, "SELECT * FROM config")
 
-
-    def get_games_from_field(self, tournament_id: str, field_id: str) -> list|bool|None:
+    def get_games_from_field(self, tournament_id: str, field_id: str) -> Union[List, bool, None]:
         """Returns games for a specific field."""
 
         return self.query(tournament_id, "SELECT * FROM games WHERE field_id IS ?", [field_id])
 
-
-    def get_game_score(self, tournament_id: str, game_id: str) -> list|bool|None:
+    def get_game_score(self, tournament_id: str, game_id: str) -> Union[List, bool, None]:
         """Returns the score for a specific game."""
-        
-        return self.query(tournament_id, "SELECT score1, score2 FROM games WHERE id IS ?", [game_id])
 
+        return self.query(tournament_id, "SELECT score1, score2 FROM games WHERE id IS ?", [game_id])
 
     def set_game_score(self, tournament_id: str, game_id: str, score: list[int]) -> bool:
         """Updates the score of a game and returns whether it was successful."""
 
         return self.query(tournament_id, "UPDATE games SET score1 = ?, score2 = ? WHERE id = ?", [score[0], score[1], game_id]) is True
 
-
-    def get_teams(self, tournament_id: str) -> list|bool|None:
+    def get_teams(self, tournament_id: str) -> Union[List, bool, None]:
         """Returns number of teams per group."""
 
         return self.query(tournament_id, "SELECT group_id, COUNT(*) AS team_count FROM teams GROUP BY group_id")
 
-
-    def get_fields(self, tournament_id: str) -> list|bool|None:
+    def get_fields(self, tournament_id: str) -> Union[List, bool, None]:
         """Returns all unique field IDs."""
 
         return self.query(tournament_id, "SELECT DISTINCT field_id FROM games")
