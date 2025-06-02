f9909a0a90ccca1a58c4c6a8c8904c15b4a2f78a
joshua-cloud-dot <s87568@informatik.htw-dresden.de>
2025-06-02 10:46:07 +0200

    rm backend folder, not necessary

--- src/backend/data/utils.py
+++
@@ -1,9 +0,0 @@
-import os
-
-DATABASE_PATH = os.path.join("data","databases")
-DEBUG = True
-
-class Error:
-    def __init__(self,code:int,message:str) -> None:
-        self.code = code
-        self.message = message
