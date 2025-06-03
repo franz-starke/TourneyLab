f8a6c79289fb92a424c4e39eb3d805a4713d63af
joshua-cloud-dot <s87568@informatik.htw-dresden.de>
2025-06-03 15:00:12 +0200

    add Todo's for frontend
    made tournamentHome Header scrollable for long names

--- src/frontend-todos.md
+++ src/frontend-todos.md
@@ -8,3 +8,5 @@
 + App Sprache auf englisch ändern können
 + Games.vue und Games stylen, Edit Games entfernen oder schöner machen
 + wenn man auf Home navbutton bei Tournament home clickt während man im Dashboard ist soll sich das normale Dashboard öffnen
++ automatic scrolling through long tournament name in TournamentHome Header
++ API richtig einsetzen. Intevalmäßig, oder jedesmal, wenn jemand den Punktestand ändert. Aktuell ist immer sobald sich die Punkte ändern
--- src/frontend/src/components/TournamentHome.vue
+++ src/frontend/src/components/TournamentHome.vue
@@ -4,17 +4,47 @@ import IconHome from "@/components/icons/IconHome.vue";
 import IconTournTree from "@/components/icons/IconTournTree.vue";
 import IconSettings from "./icons/IconSettings.vue";
 import IconTeam from "./icons/IconTeam.vue";
+import { ref, onMounted } from "vue";
 
 // TODO: first fetch from api for newest tournamentData and write it into the store
 // if offline just use store
 
 const store = useTournamentStore();
+
+
+// FIXME: this is an ugly auto scroll
+// const tournamentName = ref(null);
+// onMounted(() => {
+//   let scrollDirection = 1;
+
+//   setInterval(() => {
+//     // Scroll 1px every 100ms
+//     tournamentName.value.scrollLeft += scrollDirection;
+
+//     // Reverse direction when reaching left or right end
+//     if (
+//       tournamentName.value.scrollLeft + tournamentName.value.clientWidth >= tournamentName.value.scrollWidth ||
+//       tournamentName.value.scrollLeft <= 0
+//     ) {
+//       scrollDirection *= -1;
+//     }
+//   }, 10);
+// });
+
+
+
 </script>
 
 <template>
   
   <header id="header" class="w-full">
-    <h1 id="tournament-name" class="text-center ">
+    <h1
+      id="tournament-name"
+      class="text-center overflow-x-auto whitespace-nowrap max-w-full inline-block"
+      
+      ref="tournamentName"
+      @mounted="() => { tournamentName.scrollLeft = tournamentName.scrollWidth; }"
+    >
       {{ store.tournament.name }}
     </h1>
   </header>
@@ -66,9 +96,9 @@ const store = useTournamentStore();
   font-size: 2em;
   font-weight: bold;
   color: #333;
+  padding: 0.7em;
+
 }
-
-
 
 #tournament-views {
   width: 100%;
@@ -79,6 +109,9 @@ const store = useTournamentStore();
 .default.btn {
     transition: background-color 0s;
  
+}
+#tournament-name {
+  scroll-behavior: smooth;
 }
 
 
