d3627f4f7ef48090ae0c0789aab3a6b0abe63029
franz-starke <franzstarke@yahoo.com>
2025-06-08 16:41:17 +0200

    Added custom variables for landing page

--- src/frontend/src/components/LandingPage.vue
+++ src/frontend/src/components/LandingPage.vue
@@ -18,31 +18,23 @@ import IconEnter from '@/components/icons/IconEnter.vue';
 		</header>
 
 		<main class="flex flex-col flex-1 justify-center items-center gap-8 px-8">
-			<RouterLink to="/enter-tournament"
-				class="flex flex-col items-center justify-center w-full aspect-square rounded-4xl bg-gradient-to-br from-[#5aa49c] to-[#5aa49c] group">
+			<RouterLink to="/enter-tournament" class="enterButton group">
 				<IconEnter class="w-50 h-50 text-white mb-2 group-hover:text-black duration-200 ease-in-out" />
-				<span class="text-xl font-bold text-white group-hover:text-black duration-200 ease-in-out">Turnier
-					beitreten</span>
+				<span class="text-xl font-bold text-white group-hover:text-black duration-200 ease-in-out">
+					Turnier beitreten
+				</span>
 			</RouterLink>
 
-			<RouterLink to="/create-tournament"
-				class="relative overflow-hidden group flex items-center justify-center w-full h-20 rounded-4xl bg-white transition-colors duration-300">
+			<RouterLink to="/create-tournament" class="landingButton group">
 				<span class="text-xl font-bold z-10 transition-colors duration-300 group-hover:text-white">
 					Neue Turniere Erstellen
 				</span>
-				<div
-					class="absolute inset-0 bg-gradient-to-br from-[#5aa49c] to-[#5aa49c] opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-4xl">
-				</div>
 			</RouterLink>
 
-			<RouterLink to="/old-tournament"
-				class="relative overflow-hidden group flex items-center justify-center w-full h-20 rounded-4xl bg-white transition-colors duration-300">
+			<RouterLink to="/old-tournament" class="landingButton group">
 				<span class="text-xl font-bold z-10 transition-colors duration-300 group-hover:text-white">
 					Alte Turniere Einsehen
 				</span>
-				<div
-					class="absolute inset-0 bg-gradient-to-br from-[#5aa49c] to-[#5aa49c] opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-4xl">
-				</div>
 			</RouterLink>
 		</main>
 	</div>
