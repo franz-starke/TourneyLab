4ee3e5a505d30c464ff44e905ebdeb0510b8645c
franz-starke <franzstarke@yahoo.com>
2025-06-08 04:20:05 +0200

    Updated landing page

--- src/frontend/src/components/LandingPage.vue
+++ src/frontend/src/components/LandingPage.vue
@@ -6,30 +6,44 @@ import IconEnter from '@/components/icons/IconEnter.vue';
 </script>
 
 <template>
-	<header class="flex items-center justify-between">
-		<div class="flex items-center gap-4 ">
-			<IconFlagGerman />
-			<IconFlagUSA />
-		</div>
-		<div class="flex items-center  justify-end ">
-			<IconMoon />
-		</div>
-	</header>
+	<div class="flex flex-col min-h-[100svh] overflow-hidden touch-none overscroll-none">
+		<header class="flex items-center justify-between p-4">
+			<div class="flex items-center gap-4">
+				<IconFlagGerman />
+				<IconFlagUSA />
+			</div>
+			<div class="flex items-center justify-end">
+				<IconMoon />
+			</div>
+		</header>
 
-	<main class="flex flex-col items-stretch justify-center gap-8 ">
-		<RouterLink class="flex flex-col items-center  highlighted-btn" to="/enter-tournament">
-			<IconEnter />
-			<span>Turnier beitreten</span>
-		</RouterLink>
+		<main class="flex flex-col flex-1 justify-center items-center gap-8 px-8">
+			<RouterLink to="/enter-tournament"
+				class="flex flex-col items-center justify-center w-full aspect-square rounded-4xl bg-gradient-to-br from-[#5aa49c] to-[#5aa49c] group">
+				<IconEnter class="w-50 h-50 text-white mb-2 group-hover:text-black duration-200 ease-in-out" />
+				<span class="text-xl font-bold text-white group-hover:text-black duration-200 ease-in-out">Turnier
+					beitreten</span>
+			</RouterLink>
 
-		<RouterLink class="default-btn" to="/create-tournament">
-			<span>Neues Turnier</span>
-		</RouterLink>
+			<RouterLink to="/create-tournament"
+				class="relative overflow-hidden group flex items-center justify-center w-full h-20 rounded-4xl bg-white transition-colors duration-300">
+				<span class="text-xl font-bold z-10 transition-colors duration-300 group-hover:text-white">
+					Neue Turniere Erstellen
+				</span>
+				<div
+					class="absolute inset-0 bg-gradient-to-br from-[#5aa49c] to-[#5aa49c] opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-4xl">
+				</div>
+			</RouterLink>
 
-		<RouterLink class="default-btn" to="/old-tournament">
-			<span>Alte Turniere</span>
-		</RouterLink>
-	</main>
+			<RouterLink to="/old-tournament"
+				class="relative overflow-hidden group flex items-center justify-center w-full h-20 rounded-4xl bg-white transition-colors duration-300">
+				<span class="text-xl font-bold z-10 transition-colors duration-300 group-hover:text-white">
+					Alte Turniere Einsehen
+				</span>
+				<div
+					class="absolute inset-0 bg-gradient-to-br from-[#5aa49c] to-[#5aa49c] opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-4xl">
+				</div>
+			</RouterLink>
+		</main>
+	</div>
 </template>
-
-<style scoped></style>
