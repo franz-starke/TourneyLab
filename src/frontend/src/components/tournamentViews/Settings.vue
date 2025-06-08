ec8156d97f6944ca629e7b2185a03f2b4e89220b
franz-starke <franzstarke@yahoo.com>
2025-06-08 22:41:01 +0200

    Added settings page

--- src/frontend/src/components/tournamentViews/Settings.vue
+++ src/frontend/src/components/tournamentViews/Settings.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { useRouter } from "vue-router";
-import LandingPage from "../LandingPage.vue";
-
+import LanguageSelectBtn from "../utilcomponents/LanguageSelectBtn.vue";
+import IconMoon from "../icons/IconMoon.vue";
+import IconSun from "../icons/IconSun.vue";
+import IconLeave from "../icons/IconLeave.vue";
 
 const router = useRouter();
 
@@ -9,21 +11,43 @@ function exitTournament() {
 	// Logic to exit the tournament can be added here if needed
 	router.push({ name: "landing" });
 }
-
-
 </script>
 
 <template>
+	<div class="flex flex-col w-full h-full justify-start items-center gap-4">
+		<div class="settingsSection">
+			<span>App Sprache:</span>
+			<LanguageSelectBtn />
+		</div>
 
-	<div class="button" @click="exitTournament">
-		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-left"
-			viewBox="0 0 16 16">
-			<path fill-rule="evenodd"
-				d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z" />
-			<path fill-rule="evenodd"
-				d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708z" />
-		</svg>
-		Exit Tournament
+		<div class="settingsSection">
+			<span>App Aussehen:</span>
+			<div class="flex flex-row gap-4">
+				<div class="settingsTheme text-white bg-[var(--color-accent)]">
+					<IconSun />
+				</div>
+				<div class="settingsTheme text-black bg-white">
+					<IconMoon />
+				</div>
+			</div>
+		</div>
 
+		<div class="settingsSection">
+			<span>Akzent Farbe:</span>
+			<div class="flex flex-row gap-4">
+				<div class="settingsAccentColor bg-[var(--color-green)] border-3"></div>
+				<div class="settingsAccentColor bg-[var(--color-blue)]"></div>
+				<div class="settingsAccentColor bg-[var(--color-red)]"></div>
+				<div class="settingsAccentColor bg-[var(--color-purple)]"></div>
+			</div>
+		</div>
+
+		<div class="settingsSection w-full">
+			<span class="font-bold text-xl mb-2">Turnier Verlassen:</span>
+			<div class="flex w-full h-18 justify-evenly items-center text-2xl font-bold bg-red-500 text-white rounded-3xl" @click="exitTournament">
+				<span class="mb-0!">Turnier Verlassen</span>
+				<IconLeave />
+			</div>
+		</div>
 	</div>
 </template>
