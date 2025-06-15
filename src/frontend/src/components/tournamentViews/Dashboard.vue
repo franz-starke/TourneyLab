386faeba92eaaec485824f6f8c877db2631b6b2b
joshua-cloud-dot <s87568@informatik.htw-dresden.de>
2025-06-15 15:33:48 +0200

    added overflow-y-auto to each Dashboard view
    Added activeGroupButton highlight change on click

--- src/frontend/src/components/tournamentViews/Dashboard.vue
+++ src/frontend/src/components/tournamentViews/Dashboard.vue
@@ -78,67 +78,108 @@ function setActiveGroup(groupIndex) {
 </script>
 
 <template>
-  <div>
-    <!-- Tournament Dashboard -->
-    <div v-if="evalShow" id="eval-dialog" class="p-2">
-      <div class="flex items-center justify-evenly mb-4">
-        <button class="default-btn rounded-full px-6 py-2 bg-green-500 text-white font-semibold" style="background-color: #63a69f;" @click="setActiveGroup(0)">Fun</button>
-        <button v-if="leaderboard.groups.length == 2"
-                class="default-btn rounded-full px-6 py-2 bg-gray-200 font-semibold"
-                @click="setActiveGroup(1)">Schwitzer</button>
-      </div>
-      <div class="grid grid-cols-1 gap-3">
-        <div class="grid grid-cols-4 font-semibold bg-white rounded-xl py-2 px-4 text-center shadow-md">
-          <div>{{ $t('result.placement') }}</div>
-          <div>{{ $t('result.team') }}</div>
-          <div>{{ $t('result.win') }}</div>
-          <div>{{ $t('result.points') }}</div>
-        </div>
-        <div v-for="team in leaderboard.groups[activeGroup].teams"
-             class="grid grid-cols-4 items-center bg-white rounded-xl py-2 px-4 text-center shadow-md">
-          <div class="rounded-full w-8 h-8 mx-auto flex items-center justify-center"
-               :class="{
-                 'bg-yellow-500 text-white': team.rank === 1,
-                 'bg-gray-300': team.rank === 2,
-                 'bg-amber-700 text-white': team.rank === 3,
-                 'bg-gray-200': team.rank > 3
-               }">
-            {{ team.rank }}
-          </div>
-          <div>{{ team.id }}</div>
-          <div>{{ team.wins }}</div>
-          <div :class="{
-                'text-green-500': team.points > 0,
-                'text-red-500': team.points < 0,
-                'text-gray-500': team.points === 0
-              }">
-            {{ team.points > 0 ? '+' : '' }}{{ team.points }}
-          </div>
-        </div>
-      </div>
-    </div>
+	<div class="h-full overflow-y-auto">
+		<!-- Tournament Dashboard -->
+		<div v-if="evalShow" id="eval-dialog" class="p-2">
+			<div class="flex items-center justify-evenly mb-4">
+				<button
+					class="cursor-pointer rounded-full px-6 py-2 font-semibold"
+					:class="
+						activeGroup === 0
+							? 'bg-[var(--color-accent)] text-[var(--color-text-alt)]'
+							: 'bg-[var(--color-element)] text-[var(--color-text)]'"
+					@click="setActiveGroup(0)"
+				>
+					Fun
+				</button>
+				<button
+					v-if="leaderboard.groups.length == 2"
+					class="cursor-pointer rounded-full px-6 py-2 font-semibold"
+					:class="
+						activeGroup === 1
+							? 'bg-[var(--color-accent)] text-[var(--color-text-alt)]'
+							: 'bg-[var(--color-element)] text-[var(--color-text)]'"
+					@click="setActiveGroup(1)"
+				>
+					{{ $t("teams.pro") }}
+				</button>
+			</div>
+			<div class="grid grid-cols-1 gap-3">
+				<div
+					class="grid grid-cols-4 font-semibold bg-white rounded-xl py-2 px-4 text-center shadow-md"
+				>
+					<div>{{ $t("result.placement") }}</div>
+					<div>{{ $t("result.team") }}</div>
+					<div>{{ $t("result.win") }}</div>
+					<div>{{ $t("result.points") }}</div>
+				</div>
+				<div
+					v-for="team in leaderboard.groups[activeGroup].teams"
+					class="grid grid-cols-4 items-center bg-white rounded-xl py-2 px-4 text-center shadow-md"
+				>
+					<div
+						class="rounded-full w-8 h-8 mx-auto flex items-center justify-center"
+						:class="{
+							'bg-yellow-500 text-white': team.rank === 1,
+							'bg-gray-300': team.rank === 2,
+							'bg-amber-700 text-white': team.rank === 3,
+							'bg-gray-200': team.rank > 3,
+						}"
+					>
+						{{ team.rank }}
+					</div>
+					<div>{{ team.id }}</div>
+					<div>{{ team.wins }}</div>
+					<div
+						:class="{
+							'text-green-500': team.points > 0,
+							'text-red-500': team.points < 0,
+							'text-gray-500': team.points === 0,
+						}"
+					>
+						{{ team.points > 0 ? "+" : "" }}{{ team.points }}
+					</div>
+				</div>
+			</div>
+		</div>
 
-    <div v-else-if="!syncGames" id="dashboard-container"
-      class="flex flex-col w-full align-center justify-between h-full">
-      <button class="colorButton" @click="toggleSyncGames">
-        <span>{{ $t('home.sync') }}</span>
-        <IconQrCode />
-      </button>
-      <button class="colorButton" @click="evalTournament">
-        <span>{{ $t('home.results') }}</span>
-        <IconTrophy />
-      </button>
-    </div>
+		<div
+			v-else-if="!syncGames"
+			id="dashboard-container"
+			class="flex flex-col w-full align-center justify-between h-full"
+		>
+			<button class="colorButton" @click="toggleSyncGames">
+				<span>{{ $t("home.sync") }}</span>
+				<IconQrCode />
+			</button>
+			<button class="colorButton" @click="evalTournament">
+				<span>{{ $t("home.results") }}</span>
+				<IconTrophy />
+			</button>
+		</div>
 
-    <div v-else id="synchronize-games-container" class="flex-container overflow-y-auto">
-      <h2>{{ $t('sync.offline') }}</h2>
-      <div id="qr-code-wrapper">
-        <qrcode-stream @detect="onDetect"></qrcode-stream>
-      </div>
-      <div class="flex flex-col items-center justify-center gap-4">
-        <h2 class="font-medium">{{ $t('sync.code') }} <span class="text-blue-500">{{ store.tournament.id }}</span></h2>
-        <qrcode-vue class="" :value="qrvalue" :size="qrSize" level="L" render-as="svg" />
-      </div>
-    </div>
-  </div>
+		<div
+			v-else
+			id="synchronize-games-container"
+			class="flex-container overflow-y-auto"
+		>
+			<h2>{{ $t("sync.offline") }}</h2>
+			<div id="qr-code-wrapper">
+				<qrcode-stream @detect="onDetect"></qrcode-stream>
+			</div>
+			<div class="flex flex-col items-center justify-center gap-4">
+				<h2 class="font-medium">
+					{{ $t("sync.code") }}
+					<span class="text-blue-500">{{ store.tournament.id }}</span>
+				</h2>
+				<qrcode-vue
+					class=""
+					:value="qrvalue"
+					:size="qrSize"
+					level="L"
+					render-as="svg"
+				/>
+			</div>
+		</div>
+	</div>
 </template>
