0ba7f8c26dd02f020d1034f0c77283969733e6e8
joshua-cloud-dot <s87568@informatik.htw-dresden.de>
2025-06-22 13:17:44 +0200

    Changed Game component to select first point input on clicking anywhere on the component.
    added Routerlink to the end of the button if user wants to go to detailed point entry (EditGame)

--- src/frontend/src/components/utilcomponents/Game.vue
+++ src/frontend/src/components/utilcomponents/Game.vue
@@ -3,6 +3,7 @@ import { RouterLink } from "vue-router";
 import { computed, ref, watch } from "vue";
 import { useTournamentStore } from "@/stores/tournamentStore";
 import api from "@/api/api.js";
+import IconEnter from "../icons/IconEnter.vue";
 
 const props = defineProps({
 	gameId: {
@@ -20,7 +21,6 @@ const referee = game[2];
 const startTime = game[3];
 const points = ref(game[4]);
 
-
 const gameRoute = computed(() => ({
 	name: "edit-game",
 	params: { gameId: props.gameId },
@@ -29,10 +29,13 @@ const gameRoute = computed(() => ({
 watch(
 	() => points.value,
 	(newPoints) => {
-
 		if (navigator.onLine) {
 			try {
-				const response = api.editGameScore(store.tournament.id, props.gameId, newPoints);
+				const response = api.editGameScore(
+					store.tournament.id,
+					props.gameId,
+					newPoints
+				);
 			} catch (error) {
 				console.error("get old tournaments failed");
 			}
@@ -45,46 +48,68 @@ watch(
 	{ deep: true }
 );
 
-
-const numberInput1 = ref(null)
+const numberInput1 = ref(null);
 function selectInput1() {
-	numberInput1.value?.select()
+	numberInput1.value?.select();
 }
 
-const numberInput2 = ref(null)
+const numberInput2 = ref(null);
 function selectInput2() {
-	numberInput2.value?.select()
+	numberInput2.value?.select();
 }
 </script>
 
 <template>
-	<RouterLink :to="gameRoute">
-		<div class="flex flex-col w-full justify-center items-center">
-			<p class="font-bold text-gray-500">{{ startTime }}</p>
-			<div class="null-game" v-if="team1 == 0 || team2 == 0">--</div>
-			<div class="flex flex-row justify-between items-center w-full px-4 py-2 bg-[var(--color-element)] rounded-3xl"
-				v-else>
-				<div class="flex flex-col">
-					<div>
-						<p class="text-lg font-bold">{{ $t('games.team') }} {{ team1 }} vs. {{ $t('games.team') }} {{
-							team2 }}</p>
-					</div>
-
-					<p class="text-sm font-bold text-gray-500">{{ $t('games.ref') }}: {{ $t('games.team') }} {{ referee
-					}}</p>
+	<div class="flex flex-col w-full justify-center items-center">
+		<p class="font-bold text-gray-500">{{ startTime }}</p>
+		<div class="null-game" v-if="team1 == 0 || team2 == 0">--</div>
+		<div
+			v-else
+			class="flex flex-row justify-between items-center w-full px-4 py-2 bg-[var(--color-element)] rounded-3xl"
+			@click="selectInput1()"
+		>
+			<div class="flex flex-col">
+				<div>
+					<p class="text-lg font-bold">
+						{{ $t("games.team") }} {{ team1 }} vs.
+						{{ $t("games.team") }} {{ team2 }}
+					</p>
 				</div>
 
+				<p class="text-sm font-bold text-gray-500">
+					{{ $t("games.ref") }}: {{ $t("games.team") }}
+					{{ referee }}
+				</p>
+			</div>
+
+			<div class="flex space-x-4 items-center">
 				<div
-					class="flex flex-row w-30 justify-evenly items-center bg-[var(--color-sub-element)] p-2 rounded-full">
-					<input class="flex w-8 text-center text-2xl font-bold" ref="numberInput1" min="0" type="number"
-						v-model.number="points[0]" @click.stop.prevent @click="selectInput1()"
-						@focus="selectInput1()" />
+					class="flex flex-row w-30 justify-evenly items-center bg-[var(--color-sub-element)] p-2 rounded-full"
+				>
+					<input
+						class="flex w-8 text-center text-2xl font-bold"
+						ref="numberInput1"
+						min="0"
+						type="number"
+						v-model.number="points[0]"
+						@click.stop.prevent
+						@click="selectInput1()"
+						@focus="selectInput1()"
+					/>
 					:
-					<input class="flex w-8 text-center text-2xl font-bold" ref="numberInput2" min="0" type="number"
-						v-model.number="points[1]" @click.stop.prevent @click="selectInput2()"
-						@focus="selectInput2()" />
+					<input
+						class="flex w-8 text-center text-2xl font-bold"
+						ref="numberInput2"
+						min="0"
+						type="number"
+						v-model.number="points[1]"
+						@click.stop.prevent
+						@click="selectInput2()"
+						@focus="selectInput2()"
+					/>
 				</div>
+				<RouterLink :to="gameRoute"> <IconEnter/> </RouterLink>
 			</div>
 		</div>
-	</RouterLink>
+	</div>
 </template>
